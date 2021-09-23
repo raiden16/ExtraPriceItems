93e9185bf193775f66f5d3ddb7ba1701dd7bfe46
Luis Luna <siul-luna@hotmail.com>
2021-09-23 16:46:42 -0500

    Completo

---
+++ ExtraPriceItems/CatchingEvents.vb
@@ -0,0 +1,279 @@
+﻿Public Class CatchingEvents
+
+    Friend WithEvents SBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Friend SBOCompany As SAPbobsCOM.Company '//OBJETO COMPAÑIA
+    Friend csDirectory As String '//DIRECTORIO DONDE SE ENCUENTRAN LOS .SRF
+    Dim DocNum, Resubir As String
+
+    Public Sub New()
+
+        MyBase.New()
+        SetAplication()
+        SetConnectionContext()
+        ConnectSBOCompany()
+
+        setFilters()
+
+    End Sub
+
+    '//----- ESTABLECE LA COMUNICACION CON SBO
+    Private Sub SetAplication()
+        Dim SboGuiApi As SAPbouiCOM.SboGuiApi
+        Dim sConnectionString As String
+        Try
+            SboGuiApi = New SAPbouiCOM.SboGuiApi
+            sConnectionString = Environment.GetCommandLineArgs.GetValue(1)
+            SboGuiApi.Connect(sConnectionString)
+            SBOApplication = SboGuiApi.GetApplication()
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la aplicación SBO " & ex.Message)
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        End Try
+    End Sub
+
+    '//----- ESTABLECE EL CONTEXTO DE LA APLICACION
+    Private Sub SetConnectionContext()
+        Try
+            SBOCompany = SBOApplication.Company.GetDICompany
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con el DI")
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+            'Finally
+        End Try
+    End Sub
+
+    '//----- CONEXION CON LA BASE DE DATOS
+    Private Sub ConnectSBOCompany()
+        Dim loRecSet As SAPbobsCOM.Recordset
+        Try
+            '//ESTABLECE LA CONEXION A LA COMPAÑIA
+            csDirectory = My.Application.Info.DirectoryPath
+            If (csDirectory = "") Then
+                System.Windows.Forms.Application.Exit()
+                End
+            End If
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la BD. " & ex.Message)
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        Finally
+            loRecSet = Nothing
+        End Try
+    End Sub
+
+    '//----- ESTABLECE FILTROS DE EVENTOS DE LA APLICACION
+    Private Sub setFilters()
+        Dim lofilter As SAPbouiCOM.EventFilter
+        Dim lofilters As SAPbouiCOM.EventFilters
+
+        Try
+            lofilters = New SAPbouiCOM.EventFilters
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_LOAD)
+            lofilter.AddEx(142) '// FORMA Pedido Proveedores
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+            lofilter.AddEx("TekExtraCost")
+            lofilter.AddEx(142) '// FORMA Pedido Proveedores
+
+            SBOApplication.SetFilter(lofilters)
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("SetFilter: " & ex.Message)
+        End Try
+
+    End Sub
+
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ''// METODOS PARA EVENTOS DE LA APLICACION
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Private Sub SBO_Application_AppEvent(ByVal EventType As SAPbouiCOM.BoAppEventTypes) Handles SBOApplication.AppEvent
+        Select Case EventType
+            Case SAPbouiCOM.BoAppEventTypes.aet_ShutDown, SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition, SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged
+                System.Windows.Forms.Application.Exit()
+                End
+        End Select
+
+    End Sub
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// METODOS PARA MANEJO DE EVENTOS ITEM
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub SBOApplication_ItemEvent(ByVal FormUID As String, ByRef pVal As SAPbouiCOM.ItemEvent, ByRef BubbleEvent As Boolean) Handles SBOApplication.ItemEvent
+
+        If pVal.Before_Action = True And pVal.FormTypeEx <> "" Then
+        Else
+            If pVal.Before_Action = False And pVal.FormTypeEx <> "" Then
+                Select Case pVal.FormTypeEx
+
+                    Case 142                           '////// FORMA Factura
+                        frmORDRControllerAfter(FormUID, pVal)
+
+                    Case "TekExtraCost"
+                        frmOPORControllerAfter(FormUID, pVal)
+
+                End Select
+            End If
+        End If
+
+    End Sub
+
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// CONTROLADOR DE EVENTOS FORMA Estados de cuenta externos
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub frmORDRControllerAfter(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
+
+        Dim obotton As OPOR
+        Dim oOPOR As FrmtekOPOR
+        Dim stTabla As String
+        Dim coForm As SAPbouiCOM.Form
+        Dim oDatatable As SAPbouiCOM.DBDataSource
+        Dim stQueryH As String
+        Dim oRecSetH As SAPbobsCOM.Recordset
+        Dim OriginalPrice As Double
+        Dim Respuesta As Integer
+
+        oRecSetH = SBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        Try
+
+            Select Case pVal.EventType
+
+                '///// Carga de formas
+                Case SAPbouiCOM.BoEventTypes.et_FORM_LOAD
+                    obotton = New OPOR
+                    obotton.addFormItems(FormUID)
+
+                '//////Evento Presionar Item
+                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
+
+                    Select Case pVal.ItemUID
+
+                        Case "btCost"
+
+                            stTabla = "OPOR"
+                            coForm = SBOApplication.Forms.Item(FormUID)
+
+                            oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
+                            DocNum = oDatatable.GetValue("DocNum", 0)
+
+                            stQueryH = "Select Top 1 T1.""U_OriginalPrice"" From OPOR T0 Inner Join POR1 T1 on T1.""DocEntry""=T0.""DocEntry"" where ""DocNum""=" & DocNum & " Order by T1.""LineNum"" Asc"
+                            oRecSetH.DoQuery(stQueryH)
+
+                            If oRecSetH.RecordCount > 0 Then
+
+                                OriginalPrice = oRecSetH.Fields.Item("U_OriginalPrice").Value
+
+                                If OriginalPrice <> Nothing Or OriginalPrice <> 0 Then
+
+                                    Respuesta = SBOApplication.MessageBox("Ya se agregaron costos extra, ¿Quieres agregarlos de nuevo?", Btn1Caption:="Si", Btn2Caption:="No")
+
+                                    If Respuesta = 1 Then
+
+                                        Resubir = 1
+                                        oOPOR = New FrmtekOPOR
+                                        oOPOR.openForm(csDirectory)
+
+                                    End If
+
+                                Else
+
+                                    Resubir = 0
+                                    oOPOR = New FrmtekOPOR
+                                    oOPOR.openForm(csDirectory)
+
+                                End If
+
+                            End If
+
+                    End Select
+
+            End Select
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("Error en el evento sobre Forma Facturacion Clientes. " & ex.Message)
+        Finally
+            'oPO = Nothing
+        End Try
+    End Sub
+
+    Private Sub frmOPORControllerAfter(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
+
+        Dim coForm As SAPbouiCOM.Form
+        Dim Cost11, Cost22, Cost33, Cost44, Cost55 As String
+        Dim Cost1, Cost2, Cost3, Cost4, Cost5 As Double
+        Dim oUpOPOR As UpdateOPOR
+        Dim Respuesta As Integer
+
+        Try
+
+            Select Case pVal.EventType
+
+                '//////Evento Presionar Item
+                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
+
+                    Select Case pVal.ItemUID
+
+                        Case "1"
+
+                            Respuesta = SBOApplication.MessageBox("¿Deseas agregar los costos extra?", Btn1Caption:="Si", Btn2Caption:="No")
+
+                            If Respuesta = 1 Then
+
+                                coForm = SBOApplication.Forms.Item(FormUID)
+                                Cost11 = coForm.DataSources.UserDataSources.Item("dsCost1").Value
+                                If Cost11 = Nothing Then
+                                    Cost1 = 0
+                                Else
+                                    Cost1 = Cost11
+                                End If
+
+                                Cost22 = coForm.DataSources.UserDataSources.Item("dsCost2").Value
+                                If Cost22 = Nothing Then
+                                    Cost2 = 0
+                                Else
+                                    Cost2 = Cost22
+                                End If
+
+                                Cost33 = coForm.DataSources.UserDataSources.Item("dsCost3").Value
+                                If Cost33 = Nothing Then
+                                    Cost3 = 0
+                                Else
+                                    Cost3 = Cost33
+                                End If
+
+                                Cost44 = coForm.DataSources.UserDataSources.Item("dsCost4").Value
+                                If Cost44 = Nothing Then
+                                    Cost4 = 0
+                                Else
+                                    Cost4 = Cost44
+                                End If
+
+                                Cost55 = coForm.DataSources.UserDataSources.Item("dsCost4").Value
+                                If Cost55 = Nothing Then
+                                    Cost5 = 0
+                                Else
+                                    Cost5 = Cost55
+                                End If
+
+                                oUpOPOR = New UpdateOPOR
+                                oUpOPOR.Update(DocNum, Cost1, Cost2, Cost3, Cost4, Cost5, Resubir)
+
+                            End If
+
+                    End Select
+
+            End Select
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("Error en el evento sobre Forma Facturacion Clientes. " & ex.Message)
+        Finally
+            'oPO = Nothing
+        End Try
+    End Sub
+
+End Class
---
+++ ExtraPriceItems/UpdateOPOR.vb
@@ -0,0 +1,162 @@
+﻿Public Class UpdateOPOR
+
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private Directorio As String
+
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Directorio = oCatchingEvents.csDirectory
+    End Sub
+
+
+    Public Function Update(ByVal DocNum As String, ByVal Cost1 As Double, ByVal Cost2 As Double, ByVal Cost3 As Double, ByVal Cost4 As Double, ByVal Cost5 As Double, ByVal Resubir As String)
+
+        Dim stQueryH, stQueryH2, stQueryH3 As String
+        Dim oRecSetH, oRecSetH2, oRecSetH3 As SAPbobsCOM.Recordset
+        Dim OPOR As SAPbobsCOM.Documents
+        Dim POR1 As SAPbobsCOM.Document_Lines
+        Dim DocEntry, Lineas As Integer
+        'Dim LinePrice1, LinePrice2, CurrentPrice, Price1, Price2 As Decimal
+        Dim CurrentPrice, Price1, Price2, Price3, Price4, Price5 As Decimal
+        Dim Line, l As Integer
+        Dim scl As Integer
+        Dim SumQty, percent, LinePrice1, LinePrice2, LinePrice3, LinePrice4, LinePrice5, LstPrice1, LstPrice2, LstPrice3, LstPrice4, LstPrice5 As Double
+
+        oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH2 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH3 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        OPOR = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oPurchaseOrders)
+
+        Try
+
+            stQueryH = "Select T0.""DocEntry"",T0.""DocTotal"", count(T1.""ItemCode"") as ""Lines"", sum(T1.""Quantity"") as ""SumQty"" From OPOR T0 Inner Join POR1 T1 on T1.""DocEntry""=T0.""DocEntry"" where T0.""DocNum""=" & DocNum & " Group by T0.""DocEntry"",T0.""DocTotal"""
+            oRecSetH.DoQuery(stQueryH)
+
+            If oRecSetH.RecordCount > 0 Then
+
+                DocEntry = oRecSetH.Fields.Item("DocEntry").Value
+                Lineas = oRecSetH.Fields.Item("Lines").Value
+                SumQty = oRecSetH.Fields.Item("SumQty").Value
+
+                'LinePrice1 = Cost1 / Lineas
+                'LinePrice1 = Decimal.Round(LinePrice1, 4)
+                'LinePrice2 = Cost2 / Lineas
+                'LinePrice2 = Decimal.Round(LinePrice2, 4)
+
+                OPOR.GetByKey(DocEntry)
+                POR1 = OPOR.Lines
+
+                stQueryH3 = "Select Top 1 T0.""LineNum"" from POR1 T0 where T0.""DocEntry""=" & DocEntry & " order by ""LineNum"" Asc"
+                oRecSetH3.DoQuery(stQueryH3)
+
+                If oRecSetH3.RecordCount > 0 Then
+
+                    Line = oRecSetH3.Fields.Item("LineNum").Value
+                    l = Line + Lineas - 1
+                    scl = 0
+
+                    For i = Line To l
+
+                        stQueryH2 = "Select T0.""Quantity"",T0.""Price"",T0.""U_OriginalPrice"" from POR1 T0 where T0.""DocEntry""=" & DocEntry & " And T0.""LineNum""=" & i '& " Order by T0.""NumLine"" Desc"
+                        oRecSetH2.DoQuery(stQueryH2)
+
+                        If oRecSetH2.RecordCount > 0 Then
+
+                            percent = (oRecSetH2.Fields.Item("Quantity").Value * 100) / SumQty
+
+                            If Cost1 = 0 Then
+                                LstPrice1 = 0
+                            Else
+                                LinePrice1 = (percent * Cost1) / 100
+                                Price1 = LinePrice1 / oRecSetH2.Fields.Item("Quantity").Value
+                                LstPrice1 = Decimal.Round(Price1, 4)
+                            End If
+
+                            If Cost2 = 0 Then
+                                LstPrice2 = 0
+                            Else
+                                LinePrice2 = (percent * Cost2) / 100
+                                Price2 = LinePrice2 / oRecSetH2.Fields.Item("Quantity").Value
+                                LstPrice2 = Decimal.Round(Price2, 4)
+                            End If
+
+                            If Cost3 = 0 Then
+                                LstPrice3 = 0
+                            Else
+                                LinePrice3 = (percent * Cost3) / 100
+                                Price3 = LinePrice3 / oRecSetH2.Fields.Item("Quantity").Value
+                                LstPrice3 = Decimal.Round(Price3, 4)
+                            End If
+
+                            If Cost4 = 0 Then
+                                LstPrice4 = 0
+                            Else
+                                LinePrice4 = (percent * Cost4) / 100
+                                Price4 = LinePrice4 / oRecSetH2.Fields.Item("Quantity").Value
+                                LstPrice4 = Decimal.Round(Price4, 4)
+                            End If
+
+                            If Cost5 = 0 Then
+                                LstPrice5 = 0
+                            Else
+                                LinePrice5 = (percent * Cost5) / 100
+                                Price5 = LinePrice5 / oRecSetH2.Fields.Item("Quantity").Value
+                                LstPrice5 = Decimal.Round(Price5, 4)
+                            End If
+
+                            'Price1 = LinePrice1 / oRecSetH2.Fields.Item("Quantity").Value
+                            'LstPrice1 = Decimal.Round(Price1, 4)
+                            'Price2 = LinePrice2 / oRecSetH2.Fields.Item("Quantity").Value
+                            'LstPrice2 = Decimal.Round(Price2, 4)
+
+                            POR1.SetCurrentLine(scl)
+
+                            If Resubir = 0 Then
+                                OPOR.Lines.UserFields.Fields.Item("U_OriginalPrice").Value = oRecSetH2.Fields.Item("Price").Value
+                            End If
+
+                            OPOR.Lines.UserFields.Fields.Item("U_ExtraPrice1").Value = LstPrice1
+                            OPOR.Lines.UserFields.Fields.Item("U_ExtraPrice2").Value = LstPrice2
+                            OPOR.Lines.UserFields.Fields.Item("U_ExtraPrice3").Value = LstPrice3
+                            OPOR.Lines.UserFields.Fields.Item("U_ExtraPrice4").Value = LstPrice4
+                            OPOR.Lines.UserFields.Fields.Item("U_ExtraPrice5").Value = LstPrice5
+
+                            If Resubir = 0 Then
+                                CurrentPrice = oRecSetH2.Fields.Item("Price").Value + LstPrice1 + LstPrice2 + LstPrice3 + LstPrice4 + LstPrice5
+                            Else
+                                CurrentPrice = oRecSetH2.Fields.Item("U_OriginalPrice").Value + LstPrice1 + LstPrice2 + LstPrice3 + LstPrice4 + LstPrice5
+                            End If
+
+                            OPOR.Lines.UnitPrice = CurrentPrice
+
+                            OPOR.Lines.Add()
+
+                            scl = scl + 1
+
+                        End If
+
+                    Next
+
+                End If
+
+                OPOR.Update()
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Update. Error al actualizar el documento, " & ex.Message)
+
+        End Try
+
+    End Function
+
+
+End Class
